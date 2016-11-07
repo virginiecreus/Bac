644693589bf810076f45d683fc06c867d0b87ab0
virginiecreus <creusvirginie@gmail.com>
2016-11-07 14:56:35 +0100

    Ajout fichier

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set('display_error', 1);
+error_reporting(E_ALL);
+date_default_timezone_set('UTC');
+
+define('NOM_DB' , 'bac');
+define('UTILISATEUR_DB' , 'root');
+define('MDP_DB' , '311286');
+
+
+$bdd = mysqli_connect("localhost", "root", "311286", "bac");
+?>
