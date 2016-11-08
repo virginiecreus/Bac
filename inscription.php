610f042dcf475af9dc6b8066390f0ae8d42fd306
MFabien <fabienmehlinger.pro@gmail.com>
2016-11-08 15:03:48 +0100

    ajout de la partie inscription + fichier traitement

--- inscription.php
+++ inscription.php
@@ -1,1 +1,42 @@
-<?php include 'menu.php'?>;
+
+<?php include 'menu.php'?>
+
+
+<form action="inscription1.php" method="post">
+    <div class="form-group row">
+        <label for="example-text-input" class="col-xs-1 col-form-label">nom</label>
+        <div class="col-xs-3">
+            <input class="form-control" type="text" name="nom" placeholder="Abra">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="example-text-input" class="col-xs-1 col-form-label">prenom</label>
+        <div class="col-xs-3">
+            <input class="form-control" type="text" name="prenom" placeholder="Kadabra">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="example-text-input" class="col-xs-1 col-form-label">pseudo</label>
+        <div class="col-xs-3">
+            <input class="form-control" type="text" name="pseudo" placeholder="Alakazam">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="example-text-input" class="col-xs-1 col-form-label">Email</label>
+        <div class="col-xs-3">
+            <input class="form-control" type="email"  name="mail" placeholder="AbraKadabraAlakazam@hotmail.fr">
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="example-password-input" class="col-xs-1 col-form-label">Mot de passe</label>
+        <div class="col-xs-3">
+            <input class="form-control" type="password" name="mdp" placeholder="">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-xs-3 col-xs-offset-2">
+    <input type="submit" name="ajouter" class="btn btn-success">
+    </div>
+    </div>
+</form>
--- inscription1.php
+++ inscription1.php
@@ -1,7 +1,36 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: creus
- * Date: 08/11/16
- * Time: 09:17
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<?php include'config.php';
+// verifie si les champs sont vide et recupere les champs
+   if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['pseudo']) && !empty($_POST['mail']) && !empty($_POST['mdp'])) {
+
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $pseudo = $_POST['pseudo'];
+        $mail = $_POST['mail'];
+        $mdp = $_POST['mdp'];
+
+//insert les données
+        $req = "INSERT INTO utilisateurs (nom,prenom,mail,mot_de_passe,pseudo,role) VALUE ('$nom','$prenom','$mail','$mdp','$pseudo','membre')";
+
+// envoie a la base de donnée
+mysqli_query($bdd,$req);
+
+ echo'Ajouté';
+     header('Location: index.php ');
+
+   }
+else{
+    echo"erreur";
+}
+
+?>
+</body>
+</html>
+
