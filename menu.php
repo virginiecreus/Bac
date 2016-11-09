cde52aea424050d10234580359e08f7a3e34c2fa
MFabien <fabienmehlinger.pro@gmail.com>
2016-11-09 10:13:08 +0100

    ajout des permission pour les admin

--- menu.php
+++ menu.php
@@ -1,43 +1,50 @@
 <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <link rel="stylesheet" type="text/css" href="style.css">
-            <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        </head>
-            <body>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-3">
-                        <div class="sidebar-nav">
-                            <div class="navbar navbar-default" role="navigation">
-                                <div class="navbar-header">
-                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
-                                        <span class="sr-only">Toggle navigation</span>
-                                        <span class="icon-bar"></span>
-                                        <span class="icon-bar"></span>
-                                        <span class="icon-bar"></span>
-                                    </button>
-                                    <span class="visible-xs navbar-brand">Sidebar menu</span>
-                                </div>
-                                <div class="navbar-collapse collapse sidebar-navbar-collapse">
-                                    <ul class="nav navbar-nav">
-                                        <li class="active"><a href="inscription.php">Inscription</a></li>
-                                        <li><a href="connexion.php">Connexion</a></li>
-                                        <br>
-                                        <li><a href="index.php">Accueil</a></li>
-                                        <li><a href="Afficher_article.php">Blog</a></li>
-                                        <li><a href="profil_membre.php">Profil</a></li>
-                                        <li><a href="page_admin.php">Admin</a></li>
-                                    </ul>
-                                </div><!--/.nav-collapse -->
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-9">
 
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="sidebar-nav">
+                <div class="navbar navbar-default" role="navigation">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <span class="visible-xs navbar-brand">Sidebar menu</span>
                     </div>
+                    <div class="navbar-collapse collapse sidebar-navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="inscription.php">Inscription</a></li>
+                            <li><a href="connexion.php">Connexion</a></li>
+                            <br>
+                            <li><a href="index.php">Accueil</a></li>
+                            <li><a href="Afficher_article.php">Blog</a></li>
+                            <li><a href="profil_membre.php">Profil</a></li>
+                            <?php // ajout des permision pour les admin
+                            if ($_SESSION['role'] === 'admin') { ?>
+
+                                <li><a href="page_admin.php">Admin</a></li>
+
+                            <?php } ?>
+
+                        </ul>
+                    </div><!--/.nav-collapse -->
                 </div>
             </div>
-            </body>
-    </html>
+        </div>
+        <div class="col-sm-9">
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
