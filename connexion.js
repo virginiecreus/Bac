b5bdf3c20e012876ef9a5a6e8374469992c7aaf5
bigdjam13 <djamel.bouroub@gmail.com>
2016-11-14 16:25:09 +0100

    petite modification sur le formulaire de connexion plus page de style

---
+++ connexion.js
@@ -0,0 +1,8 @@
+/**
+ * Created by djam on 14/11/16.
+ */
+JQuery(document).ready(function(y){
+    JQuery('#lg').click(function(){
+        alert("hello");
+    });
+});
--- connexion.php
+++ connexion.php
@@ -6,27 +6,53 @@ include 'menu.php'?>;
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="bootstrap/js/bootstrap.min.js">
+    <script type="text/javascript" src="connexion.js"></script>
+
+
+
+
 </head>
     <body>
-    <form class="form-horizontal" role="form" method="post" action="connexion1.php">
-        <div class="form-group">
-            <label for="mail" class="col-sm-3 control-label">email</label>
-            <div class="col-sm-3">
-                <input type="email" class="form-control" name="mail" placeholder="email">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+    <div class="container">
+        <div class="frm-login" style="margin-top: 2px;">
+            <div class="frm-head">
+                <div class="round round-sm blue"><span class="glyphicon glyphicon-user"></span></div>
+                <label class="control-label"><h4>Page de connexion!</h4></label>
+            </div>
+            <div class="frm-body">
+                <form action="connexion1.php" method="post" class="form-horizontal">
+                    <div class="form-group">
+                        <div class="col-sm-3"><label class="control-label">Utilisateur: </label></div>
+                        <div class="col-sm-4">
+                            <input class="form-horizontal" type="text" name="mail" placeholder="@mail" required/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-3">
+                            <label class="control-label">Mot de passe: </label></div>
+                        <div class="col-sm-4">
+                            <input class="form-horizontal" type="password" name="mot_de_passe" placeholder="@mot de passe" required/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-3"></div>
+                        <div class="col-sm-2">
+                            <input class="btn btn-success" type="submit" name="login" value="LogIn" style="width: 175px;"/>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
-        <div class="form-group">
-            <label for="mot_de_passe" class="col-sm-3 control-label">mot de passe</label>
-            <div class="col-sm-3">
-                <input type="password" class="form-control" name="mot_de_passe" placeholder="mot de passe">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-offset-3 col-sm-6">
-                <button type="submit" class="btn btn-default">Se connecter</button>
-            </div>
-        </div>
-    </form>
+    </div>
+    <script>
+        JQuery(document).ready(function(y){
+            JQuery('#lg').click(function(){
+                alert("hello");
+            });
+    </script>
+
 
 
 </body>
