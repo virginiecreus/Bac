fb2eb83b1d0a5e4a6204416fe30f5539c6c2c45a
virginiecreus <creusvirginie@gmail.com>
2016-11-15 13:38:22 +0100

    Ajout photo de profil

--- Ajout_avatar1.php
+++ Ajout_avatar1.php
@@ -11,10 +11,10 @@ if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 
     if ($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        echo "ce fichier est bien une image ! " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        echo "ce fichier n'est pas une image !";
         $uploadOk = 0;
     }
 }
