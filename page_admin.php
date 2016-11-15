d739da7fa44b31c1449744e748bd0e5ddccef41a
MFabien <fabienmehlinger.pro@gmail.com>
2016-11-15 16:19:10 +0100

    modification de l'apparence

--- page_admin.php
+++ page_admin.php
@@ -10,93 +10,95 @@ include 'config.php';
 
 </head>
 <body>
-<?php include 'Liste_article.php' ;
-include 'Liste_brouillon.php'; ?>
+
 
 <div class="container">
     <div class="row">
-        <div class="col-md-offset-3">
-<!-- bouton pour acceder au modal des modification-->
-<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#ajouter">
-    Ajouter un article
-</button>
+        <div class="col-md-offset-5">
+            <!-- bouton pour acceder au modal des modification-->
+            <a type="button" class="btn btn-sm btn-info" data-toggle="modal" data-target="#ajouter">
+                <i class="glyphicon glyphicon-pencil"></i>
+            </a>
 
-<!-- Modal -->
-<div class="modal fade" id="ajouter" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 class="modal-title" id="ajouter">Ajouter un article</h4>
+            <!-- Modal -->
+            <div class="modal" id="ajouter" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <h4 class="modal-title" id="ajouter">Ajouter un article</h4>
+                        </div>
+                        <div class="modal-body">
+                            <?php include"article.php"?>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="modal-body">
-                <?php include"article.php"?>
+
+            <!-- bouton pour acceder au modal d'ajout des articles -->
+            <a type="button" class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modifier">
+                <i class="glyphicon glyphicon-file"></i>
+            </a>
+
+            <!-- Modal -->
+            <div class="modal fade" id="modifier" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <h4 class="modal-title " id="modifer">Modifier un article</h4>
+                        </div>
+                        <div class="modal-body">
+                            <?php include"modifier_article.php"?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <!-- bouton pour acceder au modal de suppresion des articles -->
+            <a type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#supprimer">
+                <i class="glyphicon glyphicon-trash"></i>
+            </a>
+
+            <!-- Modal -->
+            <div class="modal fade" id="supprimer" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <h4 class="modal-title " id="supprimer">Supprimer un article </h4>
+                        </div>
+                        <div class="modal-body">
+                            <?php include"supprimer_article.php"?>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+<br>
+<?php include 'Liste_article.php' ;
+include 'Liste_brouillon.php'; ?>
+<br>
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+            <div class="well">
+                <h2>Gestion de l'administrateur</h2>
+                <hr>
 
-<!-- bouton pour acceder au modal d'ajout des articles -->
-<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modifier">
-   Modifier un article
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="modifier" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 class="modal-title " id="modifer">Modifier un article</h4>
-            </div>
-            <div class="modal-body">
-                <?php include"modifier_article.php"?>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-<!-- bouton pour acceder au modal de suppresion des articles -->
-<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#supprimer">
-          Supprimer un article
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="supprimer" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 class="modal-title " id="supprimer">Supprimer un article </h4>
-            </div>
-            <div class="modal-body">
-                <?php include"supprimer_article.php"?>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-        <div class="container">
-            <div class="row">
-                <?php
-                $requete = "SELECT * FROM utilisateurs";
-                $exec = mysqli_query($bdd,$requete);
-                $res = array();
-                while ($row = mysqli_fetch_array($exec)) {
-                $res[] = $row;
-                ?>
-                <h2 class ="style">Gestion de l'administrateur</h2>
                 <table class="table">
                     <thead>
-                    <tr class="style">
+                    <tr>
                         <th>Nom</th>
                         <th>Prénom</th>
                         <th>Email</th>
@@ -106,28 +108,37 @@ include 'Liste_brouillon.php'; ?>
                         <th>Role</th>
                     </tr>
                     </thead>
-                    <tbody>
-                    <tr class="style">
-                        <td><?php echo $row['nom']?></td>
-                        <td><?php echo $row['prenom']?></td>
-                        <td><?php echo $row['mail']?></td>
-                        <td><?php echo $row['mot_de_passe']?></td>
-                        <td><?php echo $row['avatar']?></td>
-                        <td><?php echo $row['pseudo']?></td>
-                        <td><?php echo $row['role']?></td>
-                    </tr>
-                    </tbody>
                 </table>
+
+                <?php
+                $requete = "SELECT * FROM utilisateurs";
+                $exec = mysqli_query($bdd,$requete);
+                $res = array();
+                while ($row = mysqli_fetch_array($exec)) {
+                    $res[] = $row;
+                    ?>
+
+                    <table class="table">
+                        <tbody>
+                        <tr>
+                            <td><?php echo $row['nom']?></td>
+                            <td><?php echo $row['prenom']?></td>
+                            <td><?php echo $row['mail']?></td>
+                            <td><?php echo $row['mot_de_passe']?></td>
+                            <td><?php echo $row['avatar']?></td>
+                            <td><?php echo $row['pseudo']?></td>
+                            <td><?php echo $row['role']?></td>
+                        </tr>
+                        </tbody>
+                    </table>
+
+                    <?php
+                }
+                ?>
             </div>
-
         </div>
-        <?php
-        }
-        ?>
     </div>
-    </div>
-
-
+</div>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
--- profil.php
+++ profil.php
@@ -44,69 +44,52 @@ include 'config.php';
                                 </tbody>
                             </table>
                         </div>
-                     </div>
+                    </div>
                 </div>
                 <div class="panel-footer">
-
-                            <a data-target="#modifier" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
-                            <a data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
-                    <?php include 'Ajout_avatar.php'?>
-                </div>
+                    <!-- modale -->
+                    <a data-target="#modifier" data-original-title="Edit this user" data-toggle="modal"  class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
+                    <div class="modal fade" id="modifier" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    <h4 class="modal-title" id="modifier">Modification</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <?php include"modifier_profil.php"?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin modale -->
+                    <!-- modale -->
+                    <a data-original-title="Remove this user" data-toggle="modal" type="button" class="btn btn-sm btn-danger" data-target="#supprime"><i class="glyphicon glyphicon-remove"></i></a>
+                    <div class="modal fade" id="supprime" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    <h4 class="modal-title" id="supprime">Modification</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <?php include"supprimer_profil.php"?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin modale -->
+                </div>    <?php include 'Ajout_avatar.php'?>
             </div>
+
         </div>
     </div>
 </div>
-<div class="container">
-<div class="row">
 
-<button type="button" class="btn btn-primary btn-lg col-md-offset-5" data-toggle="modal" data-target="#modifier">
-    Modifier profil
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="modifier" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 class="modal-title" id="modifier">Modification</h4>
-            </div>
-            <div class="modal-body">
-                <?php include"modifier_profil.php"?>
-            </div>
-        </div>
-    </div>
-</div>
-    </div>
-    </div>
-
-        <br>
-<div class="container">
-    <div class="row">
-<button type="button" class="btn btn-primary btn-lg col-md-offset-5" data-toggle="modal" data-target="#supprime">
-    Supprimer profil
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="supprime" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 class="modal-title" id="supprime">Modification</h4>
-            </div>
-            <div class="modal-body">
-                <?php include"supprimer_profil.php"?>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-    </div>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
