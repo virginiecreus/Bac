ade9696f6b2a6d62444c6731ccf57dc51aedd9f4
MFabien <fabienmehlinger.pro@gmail.com>
2016-11-16 14:58:30 +0100

    modification  apparence

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ include 'config.php';
     </head>
         <body>
         <div class="container">
-            <div class="row well"> <div class="col-xs-8 col-sm-12 col-md-12">
+            <div class="row"> <div class="col-xs-8 col-sm-12 col-md-12">
                     <h2 class="text-center">Qui somme nous ?</h2>
                     <div id="myCarousel" class="vertical-slider carousel vertical slide col-md-12" data-ride="carousel">
                         <div class="row">
                             <div class="col-md-4">
                     <span data-slide="next" class="btn-vertical-slider glyphicon glyphicon-circle-arrow-up"
-                          style="font-size: 30px"></span>
+                          style="font-size: 30px; color: white"></span>
                             </div>
                             <div class="col-md-8">
                             </div>
@@ -29,8 +29,8 @@ include 'config.php';
                             <div class="item active">
                                 <div class="row">
                                     <div class="col-xs-6 col-sm-12 col-md-12">
-                                        <a href="http://dotstrap.com/"> <img src="img/virginie.jpeg" class="thumbnail center-block"
-                                                                             alt="Image" /></a>
+                                       <img src="img/virginie.jpeg" class="thumbnail center-block"
+                                                                             alt="Image" />
                                     </div>
                                     <h1 class="col-xs-6 col-sm-12 col-md-12 text-center" style="color: deeppink">
                                         Virginie
@@ -42,8 +42,8 @@ include 'config.php';
                             <div class="item ">
                                 <div class="row">
                                     <div class="col-xs-6 col-sm-12 col-md-12">
-                                        <a href="http://dotstrap.com/"> <img src="img/djamel.jpeg" class="thumbnail center-block"
-                                                                             alt="Image" /></a>
+                                         <img src="img/djamel.jpeg" class="thumbnail center-block"
+                                                                             alt="Image" />
                                     </div>
                                     <h1 class="col-xs-6 col-sm-12 col-md-12 text-center" style="color: blue">
                                        Djamel
@@ -55,8 +55,8 @@ include 'config.php';
                             <div class="item ">
                                 <div class="row">
                                     <div class="col-xs-6 col-sm-12 col-md-12">
-                                        <a href="http://dotstrap.com/"> <img src="img/fabien.jpeg" class="thumbnail center-block"
-                                                                             alt="Image" /></a>
+                                        <img src="img/fabien.jpeg" class="thumbnail center-block"
+                                                                             alt="Image" />
                                     </div>
                                     <h1 class="col-xs-6 col-sm-12 col-md-12 text-center" style="color: green">
                                         Fabien
@@ -69,7 +69,7 @@ include 'config.php';
                         <div class="row">
                             <div class="col-md-4">
                     <span data-slide="prev" class="btn-vertical-slider glyphicon glyphicon-circle-arrow-down"
-                          style="color: Black; font-size: 30px"></span>
+                          style="color: white; font-size: 30px"></span>
                             </div>
                             <div class="col-md-8">
                             </div>
