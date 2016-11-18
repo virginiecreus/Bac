403e094ada23f14ca1b5e99711582407599032b5
virginiecreus <creusvirginie@gmail.com>
2016-11-18 23:21:33 +0100

    modification de l'ajout d'article que j'ai oublié de commit

--- article.php
+++ article.php
@@ -1,63 +1,61 @@
 <?php session_start();
 include 'config.php';
 ?>
-<html>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
+    <title>Blog</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <title>Ajouter un article</title>
-
 </head>
 <body>
-<section>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 ">
-                <form name="insertion" action="article1.php" method="POST" class="form-horizontal">
-
-                    <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label font">Titre :</label>
-                        <div class="col-sm-4">
-                            <input type="text" class="form-control" name="titre" placeholder="Titre">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label">Texte :</label>
-                        <div class="col-sm-5">
-                            <textarea type="text" class="form-control" name="article" placeholder="Article"></textarea>
-                        </div>
-                    </div>
-
-                    <div   >
-
-                        <div class="col-lg-offset-1"><a>Publier :</a>
-                            <label for="name" class="control-label font">Oui</label>
-                            <input  type="radio" name="publier" value="oui" >
-
-
-                            <label for="name" class="control-label font">Non</label>
-                            <input  type="radio" name="publier" value="non" >
-                    </div>
-
-                    </div>
-
-
-
-
-
-
-                    <div class="col-md-6 col-md-offset-3">
-                        <div class="col-md-2 ">
-                            <input id="ajouter" name="ajouter" type="submit" value="Ajouter" class="btn btn-success">
-                        </div>
-                    </div>
-                </form>
-            </div>
+<h2>Nouvel article</h2>
+<form action="article1.php" method="POST" enctype="multipart/form-data" class="form-horizontal">
+    <div class="form-group">
+        <label for="name" class="col-sm-2 control-label font">Titre de l'article</label>
+        <div class="col-sm-4">
+            <input type="text" class="form-control" name="titre" placeholder="Titre" />
         </div>
     </div>
-</section>
+
+    <div class="form-group">
+        <label for="name" class="col-sm-2 control-label">Texte :</label>
+        <div class="col-sm-5">
+            <textarea type="text" class="form-control" name="article" rows="10" cols="50" placeholder="Article"></textarea>
+        </div>
+    </div>
+
+    <div>
+        <div class="col-lg-offset-1"><a>Publier :</a>
+            <label for="name" class="control-label font">Oui</label>
+            <input  type="radio" name="publier" value="oui" >
+
+
+            <label for="name" class="control-label font">Non</label>
+            <input  type="radio" name="publier" value="non" >
+        </div>
+    </div>
+
+            <input type="hidden" name="MAX_FILE_SIZE" value="2097152">
+    <p>Choisissez une photo avec une taille inférieure à 2 Mo.</p>
+    <input type="file" name="image">
+    <br /><br />
+    <input type="submit" name="ok" value="Envoyer">
+</form>
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
--- article1.php
+++ article1.php
@@ -1,34 +1,63 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
     <meta charset="UTF-8">
 </head>
 
 <body>
+<?php session_start();
+include 'config.php';
+?>
+<?php
 
-<?php include 'config.php';
+if ($_FILES['image']['error']) {
+    switch ($_FILES['image']['error']){
+        case 1: // UPLOAD_ERR_INI_SIZE
+            echo "La taille du fichier est plus grande que la limite autorisée par le serveur.";
+            break;
+        case 2: // UPLOAD_ERR_FORM_SIZE
+            echo "La taille du fichier est plus grande que la limite autorisée par le formulaire.";
+            break;
+        case 3: // UPLOAD_ERR_PARTIAL
+            echo "L'envoi du fichier a été interrompu pendant le transfert.";
 
-// si champs rempli envoyer dans la bdd
-if (isset($_POST['titre']) && isset($_POST['article'])) {
-
-// on récupére les champs du formulaire
-    $titre = addslashes($_POST['titre']);
-    $article = addslashes($_POST['article']);
-    $publier = htmlspecialchars($_POST['publier']);
-    $users = $_SESSION['id'];
-
-
-// On créé la requête
-    $req = "INSERT INTO articles (titre_article, article, utilisateurs_id, publier) 
-VALUES ('$titre', '$article', '$users', '$publier')";
-// on envoie la requête
-    $res = mysqli_query($bdd, $req);
-
-    header('Location:page_admin.php?AjoutReussi');
-} else { // si champs pas rempli erreur
-    header('Location:page_admin.php?ErreurAjout');
+            break;
+        case 4: // UPLOAD_ERR_NO_FILE
+            echo "La taille du fichier que vous avez envoyé est nulle.";
+            break;
+    }
+}
+else {
+//s'il n'y a pas d'erreur alors $_FILES['image']['error'] vaut 0
+    echo "Aucune erreur dans le transfert du fichier.<br />";
+    if ((isset($_FILES['image']['name'])&&($_FILES['image']['error'] == UPLOAD_ERR_OK))) {
+        $chemin_destination = 'uploads_articles/';
+        //déplacement du fichier du répertoire temporaire (stocké
+        //par défaut) dans le répertoire de destination
+        move_uploaded_file($_FILES['image']['tmp_name'], $chemin_destination.$_FILES['image']['name']);
+        echo "Le fichier ".$_FILES['image']['name']." a été copié dans le dossier uploads_articles";
+    }
+    else {
+        echo "Le fichier n'a pas pu être copié dans le dossier uploads_articles.";
+    }
 }
 
+$requete = "INSERT INTO articles (image, titre_article, article , utilisateurs_id, publier ) VALUES ('".$_FILES['image']['name']."','".htmlentities(addslashes($_POST['titre']), ENT_QUOTES)."','".htmlentities (addslashes($_POST['article']) , ENT_QUOTES)."','".htmlentities(addslashes($_POST['utilisateurs_id']), ENT_QUOTES)."' ,'".htmlentities(addslashes($_POST['publier']), ENT_QUOTES)."')";
+$resultat = mysqli_query($bdd,$requete);
+$identifiant = mysqli_insert_id($bdd);
+/* Fermeture de la connexion */
+mysqli_close($bdd);
+
+if ($identifiant != 0) {
+    echo "<br />Ajout de l'article réussi.<br /><br />";
+}
+else {
+    echo "<br />L'article n'a pas pu être ajouté.<br /><br />";
+}
 ?>
+<a href="page_admin.php" >retour à la page d'ajout d'articles</a>
+</body>
+</html>
     </body>
 </html>
