7cf58e13f15b968bc793f9c3fdafde71ace9644c
bigdjam13 <djamel.bouroub@gmail.com>
2016-11-19 16:46:07 +0100

    debug virginie

--- article1.php
+++ article1.php
@@ -10,10 +10,10 @@
 ?>
 
 <?php
-$id =$_SESSION['id'];
+$id = $_SESSION['id'];
 
 if ($_FILES['image']['error']) {
-    switch ($_FILES['image']['error']){
+    switch ($_FILES['image']['error']) {
         case 1: // UPLOAD_ERR_INI_SIZE
             echo "La taille du fichier est plus grande que la limite autorisée par le serveur.";
             break;
@@ -28,37 +28,32 @@ if ($_FILES['image']['error']) {
             echo "La taille du fichier que vous avez envoyé est nulle.";
             break;
     }
-}
-else {
+} else {
 //s'il n'y a pas d'erreur alors $_FILES['image']['error'] vaut 0
     echo "Aucune erreur dans le transfert du fichier.<br />";
-    if ((isset($_FILES['image']['name'])&&($_FILES['image']['error'] == UPLOAD_ERR_OK))) {
+    if ((isset($_FILES['image']['name']) && ($_FILES['image']['error'] == UPLOAD_ERR_OK))) {
         $chemin_destination = 'uploads_articles/';
         //déplacement du fichier du répertoire temporaire (stocké
         //par défaut) dans le répertoire de destination
-        move_uploaded_file($_FILES['image']['tmp_name'], $chemin_destination.$_FILES['image']['name']);
-        echo "Le fichier ".$_FILES['image']['name']." a été copié dans le dossier uploads_articles";
-    }
-    else {
+        move_uploaded_file($_FILES['image']['tmp_name'], $chemin_destination . $_FILES['image']['name']);
+        echo "Le fichier " . $_FILES['image']['name'] . " a été copié dans le dossier uploads_articles";
+    } else {
         echo "Le fichier n'a pas pu être copié dans le dossier uploads_articles.";
     }
 }
 
-$requete = "INSERT INTO articles (image, titre_article, article , utilisateurs_id, publier ) VALUES ('".$_FILES['image']['name']."','".htmlentities(addslashes($_POST['titre']), ENT_QUOTES)."','".htmlentities (addslashes($_POST['article']) , ENT_QUOTES)."','".htmlentities(addslashes($id), ENT_QUOTES)."' ,'".htmlentities(addslashes($_POST['publier']), ENT_QUOTES)."')";
-$resultat = mysqli_query($bdd,$requete);
+$requete = "INSERT INTO articles (image, titre_article, article , utilisateurs_id, publier ) VALUES ('" . $_FILES['image']['name'] . "','" . htmlentities(addslashes($_POST['titre']), ENT_QUOTES) . "','" . htmlentities(addslashes($_POST['article']), ENT_QUOTES) . "','" . htmlentities(addslashes($id), ENT_QUOTES) . "' ,'" . htmlentities(addslashes($_POST['publier']), ENT_QUOTES) . "')";
+$resultat = mysqli_query($bdd, $requete);
 
 /* Fermeture de la connexion */
 mysqli_close($bdd);
 
 if ($identifiant != 0) {
     echo "<br />Ajout de l'article réussi.<br /><br />";
-}
-else {
+} else {
     echo "<br />L'article n'a pas pu être ajouté.<br /><br />";
 }
 ?>
-<a href="page_admin.php" >retour à la page d'ajout d'articles</a>
+<a href="page_admin.php">retour à la page d'ajout d'articles</a>
 </body>
-</html>
-    </body>
 </html>
