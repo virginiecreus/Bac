132271481ba0c4b9ed4ec149e6d8652b5ff6f145
MFabien <fabienmehlinger.pro@gmail.com>
2016-11-21 11:04:01 +0100

    modification responsive

--- supprimer_profil.php
+++ supprimer_profil.php
@@ -15,7 +15,8 @@ include 'config.php';
         <div class="row">
             <div class="col-md-4 col-md-offset-1">
                 <form name="insertion" action="supprimer_profil1.php" method="POST" class="form-horizontal">
-                    <br> <p style="color: white">Etes vous sur de vouloir supprimer ce compte ?</p>
+                    <br>
+                    <p  class="col-xs-offset-2" style="color: white">Etes vous sur de vouloir supprimer ce compte ?</p>
                     <div class="col-md-6 col-md-offset-3 col-xs-offset-4">
 
                             <input id="supprimer" name="supprimer" type="submit" value="Supprimer" class="btn btn-danger">
